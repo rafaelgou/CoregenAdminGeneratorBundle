5df61633a1e4d9fba411b2016d46fc9d26cd1e61
Rafael Goulart <rafaelgou@gmail.com>
2011-11-15 11:09:16 -0200

    Filter minimal implementation 2

---
+++ Form/FilterType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Coregen\AdminGeneratorBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class FilterType extends AbstractType
+{
+    public function getName()
+    {
+        return 'filtertype';
+    }
+}
